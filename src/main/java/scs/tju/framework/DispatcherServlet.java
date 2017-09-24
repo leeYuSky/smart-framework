5f33a8cd3c927a07c85fa2bfe731c08ad820092f
liyuze <409654850@qq.com>
2017-09-24 12:35:56 +0800

    dispatcherServlet done

--- src/main/java/scs/tju/framework/DispatcherServlet.java
+++ src/main/java/scs/tju/framework/DispatcherServlet.java
@@ -1,6 +1,10 @@
 package scs.tju.framework;
 
+import scs.tju.framework.bean.*;
+import scs.tju.framework.helper.BeanHelper;
 import scs.tju.framework.helper.ConfigHelper;
+import scs.tju.framework.helper.ControllerHelper;
+import scs.tju.framework.util.*;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
@@ -11,6 +15,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.Method;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Author: liyuze
@@ -36,6 +45,74 @@ public class DispatcherServlet extends HttpServlet{
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.service(req, resp);
+        // 获取请求方法与请求路径
+        String requestMethod  = req.getMethod().toLowerCase();
+        String requestPath = req.getPathInfo();
+        // 获取 Action 处理器
+        Handler handler = ControllerHelper.getHandler(requestMethod,requestPath);
+        if(handler != null){
+            // 获取 Controller 类及其 Bean实例
+            Class<?> controllerClass = handler.getControllerClass();
+            Object controllerBean = BeanHelper.getBean(controllerClass);
+            // 创建请求参数对象
+            Map<String,Object> paramMap = new HashMap<String, Object>();
+            Enumeration<String> paramNames = req.getParameterNames();
+            while(paramNames.hasMoreElements()){
+                String paramName = paramNames.nextElement();
+                String paramValue = req.getParameter(paramName);
+                paramMap.put(paramName,paramValue);
+            }
+
+            String body = CodeUtil.decodeURL(StreamUtil.getString(req.getInputStream()));
+            if(StringUtil.isNotEmpty(body)){
+                String[] params = body.split("&");
+                if(ArrayUtil.isNotEmpty(params)){
+                    for(String param:params){
+                        String[] array = param.split("=");
+                        if(ArrayUtil.isNotEmpty(array) && array.length == 2){
+                            String paramName = array[0];
+                            String paramValue = array[1];
+                            paramMap.put(paramName,paramValue);
+                        }
+                    }
+                }
+            }
+            Param param = new Param(paramMap);
+            // 处理 Action 方法
+            Method actionMethod = handler.getActionMethod();
+            Object result = ReflectionUtil.invokeMethod(controllerBean,actionMethod,param);
+            // 处理 Action 方法返回值
+            if(result instanceof View){
+                // 返回 jsp 页面
+                View view = (View) result;
+                String  path = view.getPath();
+                if(StringUtil.isNotEmpty(path)){
+                    if(path.startsWith("/")){
+                        resp.sendRedirect(req.getContextPath()+path);
+                    } else {
+                        Map<String,Object> model = view.getModel();
+                        for(Map.Entry<String,Object> entry : model.entrySet()){
+                            req.setAttribute(entry.getKey(),entry.getValue());
+                        }
+                        req.getRequestDispatcher(ConfigHelper.getAppJspPath() + path).forward(req,resp);
+                    }
+                }
+            } else if (result instanceof Data) {
+                // 返回 JSON 数据
+                Data data = (Data) result;
+                Object model = data.getModel();
+                if(model != null){
+                    resp.setContentType("application/json");
+                    resp.setCharacterEncoding("UTF-8");
+                    PrintWriter writer = resp.getWriter();
+                    String json = JsonUtil.toJson(model);
+                    writer.write(json);
+                    writer.flush();
+                    writer.close();
+                }
+            }
+
+
+        }
     }
 }
